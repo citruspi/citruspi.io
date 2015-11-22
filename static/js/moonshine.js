1bce98d83503cba3634ef158199be006f9b66042
Mihir Singh <git.service@mihirsingh.com>
2015-11-22 04:36:00 -0500

    Set Leaflet map markers via HTML element data attributes

--- static/js/moonshine.js
+++ static/js/moonshine.js
@@ -9,10 +9,16 @@ window.onload = function() {
         window.menuOpen = !window.menuOpen;
     }
 
+    /*
     var main = new L.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: 'Map data © OpenStreetMap contributors',
         minZoom: 5,
         maxZoom: 18,
+    });
+    */
+
+    var main = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012',
     });
 
     var mapElement = document.getElementById('map')
@@ -22,5 +28,14 @@ window.onload = function() {
                 .setView([mapElement.dataset.lat, mapElement.dataset.long],
                          mapElement.dataset.zoom);
 
-    L.marker([mapElement.dataset.lat, mapElement.dataset.long]).addTo(map);
+    markers = mapElement.dataset.markers;
+
+    if (markers !== undefined) {
+        markers = markers.split(',');
+        markers.splice(markers.length-1, 1);
+
+        for (i = 0; i < markers.length; i+=2) {
+            L.marker([markers[i], markers[i+1]]).addTo(map);
+        }
+    }
 }
