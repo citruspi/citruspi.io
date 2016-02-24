6ac3e0bd448b81dfd55be2e7acf7e0008bee5ac0
Mihir Singh <git.service@mihirsingh.com>
2016-02-24 18:02:20 -0500

    Loaded the Leaflet map tiles over HTTPS

--- static/js/map.js
+++ static/js/map.js
@@ -1,5 +1,5 @@
 function configureMaps() {
-    var main = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+    var main = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
         attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012',
     });
 
